9ed529f64069fd95e3ccc69d69f7260e4f0baa5c
Jari Hanhela <jarihanhela@gmail.com>
2024-04-06 21:50:06 +0300

    Add note why CsPlayer class is not abstract

--- MertaScript/EventHandling/CsPlayer.cs
+++ MertaScript/EventHandling/CsPlayer.cs
@@ -1,5 +1,6 @@
 namespace MertaScript.EventHandling;
 
+// NOTE: This class cannot be abstract because an instance is created via JSON serialization.
 public record CsPlayer(string Name, string[] Aliases) {
   public List<string> PlayerNameWithAliases() {
     var aliasesWithPlayerName = new List<string> { Name };
