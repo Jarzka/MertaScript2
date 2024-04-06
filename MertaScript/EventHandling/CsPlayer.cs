7376849ec2303b0700f7e61770628d62ea957310
Jari Hanhela <jarihanhela@gmail.com>
2024-04-06 17:46:20 +0300

    Fix incorrect "abstract" class

--- MertaScript/EventHandling/CsPlayer.cs
+++ MertaScript/EventHandling/CsPlayer.cs
@@ -1,6 +1,6 @@
 namespace MertaScript.EventHandling;
 
-public abstract record CsPlayer(string Name, string[] Aliases) {
+public record CsPlayer(string Name, string[] Aliases) {
   public List<string> PlayerNameWithAliases() {
     var aliasesWithPlayerName = new List<string> { Name };
     aliasesWithPlayerName.AddRange(Aliases);
